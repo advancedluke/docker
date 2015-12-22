bb1b6b898171787118e27de350cf9c317380a744
Inyong Ham <iyham@naver.com>
2015-12-22 18:31:24 +0900

    Create Change_Container_SSHD_Port.sh

---
+++ dockerfiles/supervisord/Change_Container_SSHD_Port.sh
@@ -0,0 +1,12 @@
+### Change SSHD port number of supervisord based sshd container
+
+container_name=cn1
+new_ssh_port=2222
+
+# Check current sshd port number
+ssh_port=$(docker exec -it $container_name netstat -apt | grep sshd | grep -v 'tcp6' | awk '{print $4}' | grep -o '[0-9]\+')
+
+# Change port number in sshd configuration file
+docker exec -it $container_name sed -i "s/Port $ssh_port/Port $new_ssh_port/g" /etc/ssh/sshd_config
+
+# restart container

9bf24e1b8956c1b22a5b333afc1a7df2bc79f008
Inyong Ham <iyham@naver.com>
2016-01-05 11:46:35 +0900

    Create Create_ContainerUser.sh

---
+++ management/Create_ContainerUser.sh
@@ -0,0 +1,13 @@
+export container=cn1
+export user=ubuntu
+export password=changeme!!@@
+
+## Create User
+docker exec $container useradd $user -m -s /bin/bash
+
+## Change Password
+docker exec $container sh -c "echo $user':'$password | chpasswd"
+
+## Give user a sudoer permission
+docker exec $container sh -c "echo $user ALL='('ALL')' NOPASSWD':'ALL > /etc/sudoers.d/$user"
+docker exec $container chmod 0440 /etc/sudoers.d/$user
